bd50a37b535399424ff9016f6ddcdfb71e356cec
Paul Varner <varnerpaul1@gmail.com>
2022-07-02 10:48:11 -0700

    find pivot index

---
+++ find_pivot_index.py
@@ -0,0 +1,21 @@
+#LeetCode: 724. Find Pivor Index
+# Date: 7/2/2022
+
+class Solution:
+    def pivotIndex(self, nums: List[int]) -> int:
+        
+        leftSum = 0
+        arrSum = 0
+        
+        # get the sum total of the array
+        for num in nums:
+            arrSum = arrSum + num
+        
+        for index in range(len(nums)):
+            if leftSum == (arrSum - nums[index]) - leftSum:
+                return index
+            else:
+                leftSum = leftSum + nums[index]
+                
+        return -1
+            
