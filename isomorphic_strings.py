47f4fc5b9798f71c59a5d1cafc89a140c8a3ce8e
Paul Varner <varnerpaul1@gmail.com>
2022-07-03 16:05:54 -0700

    isomorphic stings

---
+++ isomorphic_strings.py
@@ -0,0 +1,39 @@
+class Solution:
+    
+      
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        checkedIndexes = []
+        matchList = []
+        matchList2 = []
+        
+        if len(s) != len(t):
+            return False
+        
+        for index in range(len(s)):
+
+            if index not in checkedIndexes:
+                for i in range(index,len(s)):
+                    if s[index] == s[i]:
+                        matchList.append(i)
+
+                for i in range(index,len(t)):
+                    if t[index] == t[i] :
+                        matchList2.append(i)
+                
+                if matchList != matchList2:
+                    return False
+
+                checkedIndexes.extend(matchList)
+                matchList = []
+                matchList2 = []
+        return True
+
+solution = Solution()
+val = solution.isIsomorphic("egg", "add")
+print(val)
+val = solution.isIsomorphic("foo", "bar")
+print(val)
+val = solution.isIsomorphic("paper", "title")
+print(val)
+val = solution.isIsomorphic("badc", "baba")
+print(val)
